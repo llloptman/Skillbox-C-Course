2fbd3cc40312a6ab69be7e75792589ec2c33b26d
LLlOPT <34040812+llloptman@users.noreply.github.com>
2021-11-28 22:04:30 +0300

    first Home Work

---
+++ first project/HeloWorldConsoleApp/HeloWorldConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HeloWorldConsoleApp
+{
+    class Program
+    {/// <summary>
+    /// Magic starts here
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!!! with WriteLine");
+            Write("Helo ");
+            Write("World ");
+            Write("!!! ");
+            Write("With Write ");
+            ReadLine();
+        }
+    }
+}
