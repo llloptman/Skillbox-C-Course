79d7c1c73abe20bc5b8cc453e946f2a34b2ed0ce
LLlOPT <34040812+llloptman@users.noreply.github.com>
2021-12-10 16:08:15 +0300

    started

---
+++ HW 4/HWOptionalTask2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWOptionalTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("FFFFF");
+            Console.ReadKey();
+        }
+    }
+}
