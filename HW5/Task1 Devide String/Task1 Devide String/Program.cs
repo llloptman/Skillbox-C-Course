f5ef18dcc541b3c62cbac43a33e2245b95f7d518
LLlOPT <34040812+llloptman@users.noreply.github.com>
2022-08-12 17:18:18 +0300

    done

---
+++ HW5/Task1 Devide String/Task1 Devide String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Devide_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string longSentence = Console.ReadLine();
+
+            ShowWords(DevideWordsBySpace(longSentence));
+            Console.ReadKey();
+
+
+
+        }
+        static string [] DevideWordsBySpace (string longSentence)
+        {
+            return longSentence.Split(' ');
+        }
+        static void ShowWords(string[] arrayOfWords)
+        {
+            foreach (var item in arrayOfWords)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
