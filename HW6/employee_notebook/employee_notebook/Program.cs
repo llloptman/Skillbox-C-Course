600db6ff92d1b8abeffb72991d342e0380607012
LLlOPT <34040812+llloptman@users.noreply.github.com>
2022-08-12 22:01:11 +0300

    done

---
+++ HW6/employee_notebook/employee_notebook/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_notebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu("employees notebook.txt");
+            Console.ReadKey();
+        }
+
+        static void WriteToFile(string fileName)
+        {
+
+                using (StreamWriter sw = File.AppendText(fileName))
+                {
+
+                    string note = string.Empty;
+                    Console.WriteLine("\n Введите id сотрудника");
+                    note += $"{Console.ReadLine(),3}#";
+                    string now = DateTime.Now.ToString();
+                    Console.WriteLine($"\n Подтвердите дату и время создания записи {now}");
+                    note += $"{now,20}#";
+                    Console.ReadKey();
+                    Console.WriteLine("\n Введите ФИО сотрудника");
+                    note += $"{Console.ReadLine(),15}#";
+                    Console.WriteLine("\n Введите возраст сотрудника");
+                    note += $"{Console.ReadLine(),5}#";
+                    Console.WriteLine("\n Введите рост сотрудника");
+                    note += $"{Console.ReadLine(),5}#";
+                    Console.WriteLine("\n Введите дату рождения сотрудника");
+                    note += $"{Console.ReadLine(),20}#";
+                    Console.WriteLine("\n Введите место рождения сотрудника");
+                    note += $"{Console.ReadLine(),15}";
+                    sw.Write($"{note}\n");
+                    sw.Close();
+                    Menu(fileName);
+                }
+            
+        }
+
+        static void ReadFromFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
+                {
+
+                }
+
+            }
+            MakeHeader();
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] data = line.Split('#');
+                    Console.Write($"{data[0],3}{data[1],20}{data[2],20}{data[3],5}{data[4],5}{data[5],20}{data[6],15}\n");
+                }
+                sr.Close();
+                Menu(fileName);
+            }
+        }
+
+        static void MakeHeader()
+        {
+            Console.Write($"{"id",3}{"date",20}{"FIO ",20}{"Age ",5}{"Hieght ", 5}{"birth day ", 20}{"Birth Place ",15}\n");
+        }
+        static void Menu(string fileName)
+        {
+            Console.WriteLine("Если хотите просмотреть записи - введите 1\n" +
+                "Если хотите сделать запись - введите 2");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+                ReadFromFile(fileName);
+            if (choice == "2")
+                WriteToFile(fileName);
+            else Menu(fileName);
+        }
+    }
+}
