809154fd0047acbb879e97cfffdee1c071d8d121
LLlOPT <34040812+llloptman@users.noreply.github.com>
2022-08-27 18:25:55 +0300

    repeat check complete

---
+++ HW8/HW8/Repeat check/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repeat_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            Menu(set);
+        }
+        public static void Menu(HashSet<string> set)
+        {
+            Console.WriteLine("Введите число");
+            string number = Console.ReadLine();
+            //HashSet<string> set2 = new HashSet<string>();
+            //set2.Add(number);
+            if (set.Contains(number))
+            {
+                Console.WriteLine($"число уже {number} вводилось");
+            }
+            else
+            {
+                Console.WriteLine($"Записываю число {number}");
+                set.Add(number);
+            }
+            Menu(set);
+        }
+    }
+}
