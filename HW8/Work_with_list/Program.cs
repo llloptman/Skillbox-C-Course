28bd2f84e15a58feb72e0942edc9f765ef8d98a2
LLlOPT <34040812+llloptman@users.noreply.github.com>
2022-08-27 16:51:55 +0300

    task 1 comlete

---
+++ HW8/Work_with_list/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_with_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> hundredNumers = new List<int>(100);
+            Fill100(hundredNumers);
+            ShowList(hundredNumers);
+            DeleteNubersInRange(hundredNumers);
+            ShowList(hundredNumers);
+            
+        }
+        public static void Fill100(List<int> list)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(random.Next(0,100));
+            }
+        }
+        public static void ShowList(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        public static void DeleteNubersInRange (List<int> list, int min, int max)
+        {
+            foreach (var item in list.Reverse<int>())
+            {
+
+                if (item >= min && item <= max)
+                {
+                    list.Remove(item);
+                }
+            }
+        }
+        public static void DeleteNubersInRange (List<int> list)
+        {
+            foreach (var item in list.Reverse<int>())
+            {
+
+                if (item >= 25 && item <= 50)
+                {
+                    list.Remove(item);
+                }
+            }
+           
+        }
+
+    }
+}
